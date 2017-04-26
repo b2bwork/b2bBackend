085b346c44a7287320ddf80236a6860e6865ecb0
jarupong pajakgo <lagmanmc@gmail.com>
2017-04-26 22:04:58 +0700

    [Graphql] Category and unit category schema

--- src/Schema/user.js
+++ src/Schema/user.js
@@ -1,9 +1,5 @@
 export const typeDefs =  [`
-      type Query {
-        getuser(_id: String): User
-        login(Username: String , Password: String): User
-        CheckUser(Username:String): User
-      }
+      
 
       type User {
         _id: String
@@ -14,10 +10,31 @@ export const typeDefs =  [`
         Age: Int
       }
 
+      type Category{
+        _id: String
+        Name: String 
+        Image: String 
+      }
+
+      type UnitCategory{
+        CategoryName: String
+        Name: String
+        Image: String 
+      }
+
+
+
+      type Query {
+        getuser(_id: String): User
+        login(Username: String , Password: String): User
+        CheckUser(Username:String): User
+        listCategory(): Category
+        listUnitCategory(Name: String): 
+      }
+
       type Mutation {
         register(Username: String! , Password: String! , Name: String! , BirthData: String! , Age:Int!): User
       }
-
       schema {
         query: Query
         mutation: Mutation
