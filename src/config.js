1e1584dcebe607aa102971897f859947f1b6d474
jarupong pajakgo <lagmanmc@gmail.com>
2017-05-29 22:54:35 +0700

    [PassportJS] add google secret key

--- src/config.js
+++ src/config.js
@@ -6,3 +6,4 @@ export const OMISE_SECRET_KEY = 'skey_test_57vo88hs85r8478o4oh';
 export const OMISE_PUBLIC_KEY = 'pkey_test_57vo88hrxzcpzuhzzrg';
 export const GOOGLE_CLIENT_ID = '11102905009-0s1rssjoe4in6e24e0b2se9c34ao7a1s.apps.googleusercontent.com';
 export const GOOGLE_SECRET_KEY = 'WMV1PowxHeyupSk1YDyebbzN';
+export const GOOGLE_CALLBACK_URL = 'http://example.com/subdir';
