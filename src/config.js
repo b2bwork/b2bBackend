54b73f206aaadb7b12d343ca2a9c9b9ffc03d132
jarupong pajakgo <lagmanmc@gmail.com>
2017-05-29 21:49:42 +0700

    [PassportJS] google key

--- src/config.js
+++ src/config.js
@@ -4,3 +4,5 @@ export const UPLOAD_PATH = './Images/';
 export const WEBSOCKET_PORT = 5000;
 export const OMISE_SECRET_KEY = 'skey_test_57vo88hs85r8478o4oh';
 export const OMISE_PUBLIC_KEY = 'pkey_test_57vo88hrxzcpzuhzzrg';
+export const GOOGLE_CLIENT_ID = '11102905009-0s1rssjoe4in6e24e0b2se9c34ao7a1s.apps.googleusercontent.com';
+export const GOOGLE_SECRET_KEY = 'WMV1PowxHeyupSk1YDyebbzN';
