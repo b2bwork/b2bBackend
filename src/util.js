e2f62767537624a9f0ee2d898dbf3c4f9243f44e
jarupong pajakgo <lagmanmc@gmail.com>
2017-06-16 22:51:41 +0700

    add function make list image

---
+++ src/util.js
@@ -0,0 +1,12 @@
+import {IMAGE_URL} from './config';
+
+export function listImage(listImage){
+    console.log(listImage);
+    let list = [];
+    
+    /*for(i=0;listImage.length;i++){
+        list.push(listImage[i].filename);
+    }*/
+    return list;
+
+}
